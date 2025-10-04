b25f474f2a1f1d181cb7129cccca5ac2b821b18b
danish1357 <danish.ansari1357@gmail.com>
2025-10-05 03:12:37 +0530

    Import Composable annotation

--- app/src/main/java/com/mindpin/app/QuickAddNoteActivity.kt
+++ app/src/main/java/com/mindpin/app/QuickAddNoteActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
